7646996ac9452f8f2382b74990a6926f4bba23ce
V Sivesh <vsivesh10@gmail.com>
2026-09-08 10:26:30 +0530

    Add files via upload

---
+++ identity.py
@@ -0,0 +1,25 @@
+#identity operators
+a = None
+print(a is None)
+print(a is not None)
+
+#bitwise operators
+a = 5
+b = 3
+print(a & b)
+print(a | b)
+print(a ^ b)
+
+#electricity bill calculator
+units = int(input("Enter electricity units: "))
+rate = 6
+bill = units * rate
+print("Electricity Bill:", bill)
+
+#travel expense calculator
+travel = float(input("Travel expense: "))
+food = float(input("Food expense: "))
+hotel = float(input("Hotel expense: "))
+total = travel + food + hotel
+print("Total Expense:", total)
+
