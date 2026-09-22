6a5faaed12a48b46f738159270211156b3e615de
V Sivesh <vsivesh10@gmail.com>
2026-09-22 07:13:40 +0530

    Add files via upload

---
+++ conditionstatements.py
@@ -0,0 +1,22 @@
+#if elif
+number=int(input("enter a number"))
+if (number==1):
+    print("MRDU")
+elif (number==2):
+    print("MBU")
+elif (number==3):
+    print("Gitam")
+else:
+    print("Choose another college")
+
+#nested
+number=int(input("Enter a number"))
+if(number>0):
+    if(number<50):
+        print("Number is between 1-50")
+    else:
+        print("Number is greater than 50")
+else:
+    print("-ve number")
+
+    
